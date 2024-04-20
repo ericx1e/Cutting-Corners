45f6fda4efc12b13d2bc5db4d2047d8a77c4b9d2
Kieran Khan <kierank.pc@gmail.com>
2024-04-20 16:17:30 -0400

    railway json removed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+import uvicorn
+import os
 
 app = FastAPI()
 
@@ -14,3 +16,6 @@ async def root():
 @app.post("/api/v1/drawing")
 async def create_drawing(drawing: Drawing):
     return drawing
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
