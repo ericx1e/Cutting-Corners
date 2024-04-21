cb530268f9da59fc2d93e43d0849cde6cbd0ddd8
Kieran Khan <kierank.pc@gmail.com>
2024-04-20 22:18:03 -0400

    api working

---
+++ image_classifier.py
@@ -0,0 +1,63 @@
+import torch.nn as nn
+import torch
+
+reversed_encoding = {
+        0: 'airplane',
+        1: 'car',
+        2: 'dragon',
+        3: 'crocodile',
+        4: 'computer',
+        5: 'elephant',
+        6: 'flower',
+        7: 'guitar',
+        8: 'hamburger',
+        9: 'helicopter',
+        10: 'house',
+        11: 'hurricane',
+        12: 'jail',
+        13: 'keyboard',
+        14: 'leaf',
+        15: 'mermaid',
+        16: 'microwave',
+        17: 'monkey',
+        18: 'pants',
+        19: 'broccoli',
+        20: 'brain',
+        21: 'raccoon',
+        22: 'radio',
+        23: 'rifle',
+        24: 'sink'
+    }
+
+class ImageClassifier(nn.Module):
+    def __init__(self):
+        super(ImageClassifier, self).__init__()
+        # goal: simplified Alexnet architecture
+        # input: 28x28x1 image, grayscaled
+        # first idea: 3 conv-relu-pool layers, followed by 2 fc layers and softmax non-linearity
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(1, 10, 3, 1, 'same'),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(10, 20, 3, 1, 'same'),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.linear_layers = nn.Sequential(
+            nn.Linear(980, 980),
+            nn.ReLU(),
+            nn.Dropout(p=.2),
+            nn.Linear(980, 25)
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = torch.flatten(x, start_dim=1)
+        return self.linear_layers(x)
+
+    def predict(self, x):
+        x = torch.reshape(x, (1, 1, 28, 28))
+        output = self.forward(x)
+        return reversed_encoding[torch.argmax(output, dim=1).item()]
--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from typing import List
 import uvicorn
 import os
 import torch
-import torch.nn as nn
 from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import base64
 import io
+from image_classifier import ImageClassifier, reversed_encoding
 
 app = FastAPI()
 
@@ -22,68 +22,9 @@ async def root():
 
 @app.post("/api/v1/drawing")
 async def classify_drawing(drawing: Drawing):
-    reversed_encoding = {
-        0: 'airplane',
-        1: 'car',
-        2: 'dragon',
-        3: 'crocodile',
-        4: 'computer',
-        5: 'elephant',
-        6: 'flower',
-        7: 'guitar',
-        8: 'hamburger',
-        9: 'helicopter',
-        10: 'house',
-        11: 'hurricane',
-        12: 'jail',
-        13: 'keyboard',
-        14: 'leaf',
-        15: 'mermaid',
-        16: 'microwave',
-        17: 'monkey',
-        18: 'pants',
-        19: 'broccoli',
-        20: 'brain',
-        21: 'raccoon',
-        22: 'radio',
-        23: 'rifle',
-        24: 'sink'
-    }
-
-    class ImageClassifier(nn.Module):
-        def __init__(self):
-            super(ImageClassifier, self).__init__()
-            # goal: simplified Alexnet architecture
-            # input: 28x28x1 image, grayscaled
-            # first idea: 3 conv-relu-pool layers, followed by 2 fc layers and softmax non-linearity
-
-            self.conv_layers = nn.Sequential(
-                nn.Conv2d(1, 10, 3, 1, 'same'),
-                nn.ReLU(),
-                nn.MaxPool2d(2, 2),
-                nn.Conv2d(10, 20, 3, 1, 'same'),
-                nn.ReLU(),
-                nn.MaxPool2d(2, 2)
-            )
-
-            self.linear_layers = nn.Sequential(
-                nn.Linear(980, 980),
-                nn.ReLU(),
-                nn.Dropout(p=.2),
-                nn.Linear(980, 25)
-            )
-
-        def forward(self, x):
-            x = self.conv_layers(x)
-            x = torch.flatten(x, start_dim=1)
-            return self.linear_layers(x)
-
-        def predict(self, x):
-            x = torch.reshape(x, (1, 1, 28, 28))
-            output = self.forward(x)
-            return reversed_encoding[torch.argmax(output, dim=1).item()]
-
-    model = torch.load('Model/model.pth')
+    model = ImageClassifier()
+    model.load_state_dict(torch.load("Model/model.pth"))
+    model.eval()
     preprocessed_img = torch.tensor(compress_doodle(image_base64)).float()
     prediction = model.predict(preprocessed_img)
     return prediction
